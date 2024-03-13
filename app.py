7d0981ce9b255823d02c2f802cebf27ab65c36bf
Phillip Dimitroulakos <phillip.dimitroulakos@ontariotechu.net>
2024-03-13 18:25:22 -0400

    Shows Top 5 songs, all works other than  artist name not showing

--- app.py
+++ app.py
@@ -11,17 +11,7 @@ def index():
 def search_track_route():
     if request.method == 'POST':
         songName = request.form['track']
-        results = song_results(songName)
-        return render_template('results.html', results=results)
+        all_results = song_results(songName)
+        return render_template('results.html', all_results=all_results)
     else:
         return render_template('searchSong.html')
-
-# @app.route('/searchTrack', methods=['GET', 'POST'])
-# def search_track_route():
-#     if request.method == 'POST':
-#         songName = request.form['track']  # Get the artist name from the form in the searchArtist.html template
-#         results = song_results(trackName)
-#         # songs = get_artist_songs(artistName)
-#         return render_template('results.html', results=results, songs=songs)  # Render results.html and pass variables to the template
-#     else:
-#         return render_template('searchArtist.html')
--- searchSong.py
+++ searchSong.py
@@ -3,14 +3,17 @@ from requests import get, post, request
 import json
 
 def search_for_song(token, track):
-    url = f"https://api.spotify.com/v1/search?q={track}&type=track&limit=1"
+    url = f"https://api.spotify.com/v1/search?q={track}&type=track&limit=5"
     headers = get_auth_headers(token)
 
-    result = get(url, headers=headers)
-    jsonResult = json.loads(result.content)["track"]["items"]
-    return jsonResult[0]
+    results = get(url, headers=headers)
+    jsonResult = json.loads(results.content)["tracks"]["items"]
+    return jsonResult
 
 def song_results(songName):
     token = get_token()
-    result = search_for_song(token, songName)
-    return result
+    results = search_for_song(token, songName)
+    all_results = []
+    for result in results:
+        all_results.append(result)
+    return all_results
