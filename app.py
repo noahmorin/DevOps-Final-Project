71d9c8a180b0267cb370102f194f84868ae056c6
fatimakaleem <fatima.kaleem@ontariotechu.net>
2024-03-16 01:34:23 -0400

    changed ui a bit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('minioninifier.html')
 
 @app.route('/home', methods=['GET', 'POST']) 
 def home():
