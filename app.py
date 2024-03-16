99facb8d41830ceb560013ef6234ca6fe84da74b
Braydan Brown <73270460+BraydanBrown@users.noreply.github.com>
2024-03-15 22:29:15 -0400

    renamed results.html

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ def search_artist_route():
     if request.method == 'POST':
         artistName = request.form['artist']  # Get the artist name from the form in the searchArtist.html template
         results = artist_results(artistName)
-        return render_template('results.html', results=results)  # Render results.html and pass variables to the template
+        return render_template('artistResults.html', results=results)  # Render results.html and pass variables to the template
     else:
         return render_template('searchArtist.html')
