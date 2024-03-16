b8d01e35a59f69cbc713b947fed46b80eda54c93
Phillip Dimitroulakos <phillip.dimitroulakos@ontariotechu.net>
2024-03-16 12:35:50 -0400

    Search Song tests, still failing but getting close maybe

--- test/testSearchSong.py
+++ test/testSearchSong.py
@@ -1,54 +1,30 @@
-# from auth import get_token, get_auth_headers
-# from requests import get, post, request
-# import json
-# import unittest
-# import test
-# from unittest.mock import patch, MagicMock
-
-
-from auth import get_token, get_auth_headers
-from requests import get, post, request
+import unittest
 import json
 from flask import render_template
 
 class test_searchSong(unittest.TestCase):
-    track = 'Killer Queen'
-    token = get_token()
-    def test_search_for_song(token, track):
-        url = f"https://api.spotify.com/v1/search?q={track}&type=track&limit=5"
-        headers = get_auth_headers(token)
-        results = call_api(url, headers)
+    def test_search_for_song(self):
+        results = self.call_api()
         try:
             jsonResult = json.loads(results.content)["tracks"]["items"]
         except:
             jsonResult = []
+        self.assertIsInstance(jsonResult, dict)
         return jsonResult
 
-    def call_api(url, headers):
-        return get(url, headers=headers)
+    def call_api(self):
+        with open('test.json', 'r') as f:
+            return json.load(f)
 
-    def test_song_results(songName):
-        token = get_token()
-        results = search_for_song(token, songName)
+    def test_song_results(self):
+        results = self.test_search_for_song()
         if results == []:
             return render_template('noResults.html')
         all_results = []
         for result in results:
             all_results.append(result)
+        self.assertIsInstance(all_results, list)
         return all_results
 
-
-# class test_searchSong(unittest.TestCase):
-#     def test_search_for_song(self):
-#         track = 'Killer Queen'
-#         url = f"https://api.spotify.com/v1/search?q={track}&type=track&limit=5"
-#         headers = get_auth_headers(token)
-#         with open('test.json', 'r') as f:
-#             test_data = json.load(f)
-#         mock_call_api.return_value = MagicMock(content=json.dumps(test_data))
-#         results = test
-#         try:
-#             jsonResult = json.loads(results.content)["tracks"]["items"]
-#         except:
-#             jsonResult = []
-#         return jsonResult
+if __name__ == '__main__':
+    unittest.main()
