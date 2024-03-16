a309736107a5402214e79898d3bf8b3b03a79659
Braydan Brown <73270460+BraydanBrown@users.noreply.github.com>
2024-03-15 22:51:01 -0400

    Update searchArtist.py

--- searchArtist.py
+++ searchArtist.py
@@ -5,18 +5,23 @@ import json
 def search_for_artist(token, artist):
     url = f"https://api.spotify.com/v1/search?q={artist}&type=artist&limit=1"
     headers = get_auth_headers(token)
-
     result = get(url, headers=headers)
-    jsonResult = json.loads(result.content)["artists"]["items"]
 
-    # TODO - error handling for when the artist is not found
+    if result.status_code == 200:
+        try:
+            jsonResult = json.loads(result.content)
+
+            if "artists" in jsonResult and "items" in jsonResult["artists"]:
+                return jsonResult["artists"]["items"][0]
+            
+        except ValueError:
+            raise ValueError("Unexpected JSON response from Spotify API")
     
-    return jsonResult[0]
+    else: 
+        raise Exception(f"Status Code {result.status_code} and response: {result.content}, while trying to search for artist {artist}.")
 
 def artist_results(artistName):
     token = get_token()
     result = search_for_artist(token, artistName)
-
-    # artistID = result["id"]
 
     return result
