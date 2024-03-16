29bec20d1ac530a95a3aa7914f3e907e750a8aed
Braydan Brown <73270460+BraydanBrown@users.noreply.github.com>
2024-03-15 23:42:22 -0400

    Update test_searchArtist.py

--- test/test_searchArtist.py
+++ test/test_searchArtist.py
@@ -8,12 +8,11 @@ from searchArtist import search_for_artist, artist_results
 class test_searchArtist(unittest.TestCase):
 
     def test_successful_search(self):
-        token = get_token()
-        result = search_for_artist(token, "The Minions")
+        result = artist_results("The Minions")
 
         # Check JSON response structure
         self.assertIsInstance(result, dict)
-        self.assertIn("artists", result)
-        self.assertIn("items", result["artists"])
-        self.assertTrue(len(result["artists"]["items"]) > 0)
-        self.assertEqual(result["artists"]["items"][0]["name"], "The Minions")
+        self.assertIn("name", result)
+        self.assertEqual(result["name"], "The Minions")
+        self.assertEqual(result["external_urls"]["spotify"], "https://open.spotify.com/artist/3NVrWkcHOtmPbMSvgHmijZ")
+        self.assertTrue(len(result) > 0)
