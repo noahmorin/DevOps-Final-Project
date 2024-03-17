da39bd880960cc4e6e116e8f47a08da6522188d6
Braydan Brown <73270460+BraydanBrown@users.noreply.github.com>
2024-03-16 23:20:31 -0400

    modified html templates, styling, and top tracks function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from searchArtist import artist_results
+from artistTopTracks import get_artist_top_tracks
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ def search_artist_route():
     if request.method == 'POST':
         artistName = request.form['artist']  # Get the artist name from the form in the searchArtist.html template
         results = artist_results(artistName)
-        return render_template('artistResults.html', results=results)  # Render results.html and pass variables to the template
+        songs = get_artist_top_tracks(artistName)
+        return render_template('artistResults.html', results=results, songs=songs)  # Render results.html and pass variables to the template
     else:
         return render_template('searchArtist.html')
