a79a9c6ca4a7bc8e60d348c310f25a9d77b156a6
Lathushan Pavalavelauthan <lathushan.pavalavelauthan@ontariotechu.net>
2024-03-17 19:25:46 -0400

    Updating user profile tests

--- test/test_userInfo.py
+++ test/test_userInfo.py
@@ -1,5 +1,5 @@
 import unittest
-from userInfo import user_result, user_name, user_profile_image, userPlaylist
+from userInfo import user_result, user_name, user_profile_image, user_playlist, user_top_artist, user_top_track
 from auth import get_token
 from dotenv import load_dotenv
 import os
@@ -24,11 +24,29 @@ class test_userInfo(unittest.TestCase):
         self.assertTrue([]!= testUserImage)
         self.assertEqual(200, requests.get(testUserImage).status_code)
 
-    # Uses developer token to check if a user playlist is not empty and is in dictionary format
-    def user_image_not_empty(self):
+    # Uses developer token to check if user playlist is not empty and is in dictionary format
+    def user_playlist_not_empty(self):
         load_dotenv()
         os.environ['userToken'] = get_token()
-        testUserPlaylist = user_profile_image(user_result())
+        testUserPlaylist = user_playlist(user_result())
 
         self.assertTrue([]!= testUserPlaylist)
         self.assertIsInstance(testUserPlaylist, dict)
+    
+    # Uses developer token to check if user top artist is not empty and is in dictionary format
+    def user_artist_not_empty(self):
+        load_dotenv()
+        os.environ['userToken'] = get_token()
+        testUserArtist = user_top_artist(user_result())
+
+        self.assertTrue([]!= testUserArtist)
+        self.assertIsInstance(testUserArtist, dict)
+    
+    # Uses developer token to check if user top track is not empty and is in dictionary format
+    def user_track_not_empty(self):
+        load_dotenv()
+        os.environ['userToken'] = get_token()
+        testUserTrack = user_top_track(user_result())
+
+        self.assertTrue([]!= testUserTrack)
+        self.assertIsInstance(testUserTrack, dict)
